ed28ac0516cce87c4daf759a6c92323c2e920076
nlvphu <nlvphu@gmail.com>
2024-07-07 17:52:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Word Guess</h1>
+        <WordGuess/>
+      </header>
+    </div>
+  )
+}
+
+export default App
